eef3191d441ce84b4daae2f84ad0e3f82f60f037
rirenepc <rirenepc@gmail.com>
2022-02-26 11:11:39 +0100

    cambio mapa MapBox MAPA 3D

---
+++ js/rutas.js
@@ -0,0 +1,30 @@
+   function addRutas() {
+
+            var url = 'datos/rutas.geojson';
+            map.addSource('rutas', { type: 'geojson', data: url});
+
+                 map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': 'rutas',
+            'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+            },
+            'paint': {
+            'line-color': '#ff0000',
+            'line-width': 3
+            }
+            });
+
+} //fin funcion
+/*function zoomToRutas(valores) {
+
+   var coord = valores.split("/");
+
+    map.flyTo({
+            center: [coord[2], coord[1]],
+            zoom: coord[0]
+        });
+
+    } //fin funcion*/
